1477e96d63db99eced5ddd81a45867503e07e2cf
Saurav <47532084+Saurav@users.noreply.github.com>
2020-01-22 22:31:48 +0530

    Added comments

--- app.js
+++ app.js
@@ -7,62 +7,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 
-//SCHEMA
-var campgroundSchema= new mongoose.Schema({
-	name:String,
-	image:String,
-	description:String
-});
- var Campground=mongoose.model("Campground", campgroundSchema);
+var Campground=require("./models/campground");
+var Comment=require("./models/comment");
 
+var seedDb=require("./seeds");
 
-// Campground.create({
-// 		name: "Stone dog",
-// 		image:"https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg",
-// 		description:"No cats allowed!"
-// 	},function(err, campground){
-// 	if(err)
-// 		console.log(err);
-// 	else
-// 		console.log("New Camp");
-// });
+seedDb();
 
-
-
-
-
-
-// var campgrounds=[{
-// 		name : "Tuna divine",
-// 		image:"https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	},
-// 	{
-// 		name: "Stone dog",
-// 		image:"https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	},
-// 	{
-// 		name: "Goat Mountain",
-// 		image:"https://pixabay.com/get/55e8dc404f5aab14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	},
-// 				 {
-// 		name : "Tuna divine",
-// 		image:"https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	},
-// 	{
-// 		name: "Stone dog",
-// 		image:"https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	},{
-// 		name : "Tuna divine",
-// 		image:"https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	},
-// 	{
-// 		name: "Stone dog",
-// 		image:"https://pixabay.com/get/52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722678d39f4ac25f_340.jpg"
-// 	}
-// 					];
-
-
-// app.use(express.static("public")); 
+ app.use(express.static(__dirname+"/public")); 
 
 	
 app.get("/",function(req,res){
@@ -71,7 +23,7 @@ app.get("/",function(req,res){
 
 
 app.get("/campgrounds/new",function(req,res){
-	res.render("new");
+	res.render("campgrounds/new");
 	
 });
 
@@ -82,7 +34,7 @@ app.get("/campgrounds",function(req,res){
 			console.log("err");
 			
 		}else{
-			res.render("index",{
+			res.render("campgrounds/index",{
 		campgrounds: campgrounds
 	});
 		}
@@ -110,14 +62,50 @@ app.post("/campgrounds",function(req,res){
 });
 
 app.get("/campgrounds/:id",function(req,res){
-		Campground.findById(req.params.id,function(err,foundCamp){
+		Campground.findById(req.params.id).populate("comments").exec(function(err,foundCamp){
 			if(err)
 				console.log("Error");
-			else
-				res.render("show",{campground:foundCamp});
+			else{
+				console.log(foundCamp);
+				res.render("campgrounds/show",{campground:foundCamp});
+			}
 		});
 });
 
+
+//COMMENT ROUTES
+
+app.get("/campgrounds/:id/comments/new",function(req,res){
+	Campground.findById(req.params.id,function(err,campground){
+		if(err)
+			console.log(err);
+		else{
+			res.render("comments/new",{
+				campground:campground
+			});
+		}
+	});
+	
+});
+app.post("/campgrounds/:id/comments",function(req,res){
+	Campground.findById(req.params.id,function(err,campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");
+		}
+		else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err);
+				else{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/"+req.params.id);
+				}
+			});
+		}
+	});
+});
 app.listen(3000,function(){
 	console.log("connected");
 })
---
+++ seeds.js
@@ -0,0 +1,57 @@
+var mongoose =require("mongoose");
+var Campground= require("./models/campground");
+var Comment=require("./models/comment");
+var data=[
+	{
+		name:"Cloud rest",
+		image:"https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+		description:"agag"
+	},
+	{
+		name:"Cloud active",
+		image:"https://images.unsplash.com/photo-1497906539264-eb74442e37a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+		description:"ggG"
+		
+	},
+	{
+		name:"Cloud SLEEP",
+		image:"https://images.unsplash.com/photo-1497906539264-eb74442e37a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+		description:"agaafag"
+	}
+];
+
+function seedDb(){
+	
+	//Remoe all campgrounds
+		Campground.deleteMany({},function(err){
+		if(err)
+			console.log(err);
+		console.log("Removed campgrounds");
+			
+			//add few campgrounds
+		data.forEach(function(seed){
+			Campground.create(seed,function(err,campground){
+				if(err)
+					console.log(err)
+				else
+					console.log("Added");
+				//create comment
+				Comment.create({
+					text:"This place is great",
+					author:"Homer"
+				},function(err, commen){
+					if(err)
+						console.log(err);
+					else
+						campground.comments.push(commen);
+						campground.save();
+						console.log("comment added");
+				
+				});
+			});
+			
+		});
+	});
+	
+}
+module.exports=seedDb;
